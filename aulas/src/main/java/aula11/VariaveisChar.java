cc62df7e2db7bdbc3e453b4d0a1b392ec76ac173
lopcarv <d7m7e7@gmail.com>
2025-06-06 21:36:57 -0300

    Primeiro commit 01

---
+++ aulas/src/main/java/aula11/VariaveisChar.java
@@ -0,0 +1,10 @@
+package aula11;
+
+public class VariaveisChar {
+    public static void main(String[] args) {
+        char o = 'o';
+        char i = 'i';
+        char interrogacao = 0X00E1; // valor de '?'
+        System.out.println(""+o+i+interrogacao);
+    }
+}
