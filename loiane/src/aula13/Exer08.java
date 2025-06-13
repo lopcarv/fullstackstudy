bb62ad0b6ff1cb985d42f6d2857c3390e297547e
lopcarv <d7m7e7@gmail.com>
2025-06-13 01:08:06 -0300

    Atualizado em 13jun2025 01h01

--- loiane/src/aula13/Exer08.java
+++ loiane/src/aula13/Exer08.java
@@ -6,6 +6,16 @@ public class Exer08 {
 	
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Quanto você ganha por Hora: ");
+		double ValorHora = scan.nextDouble();
+		System.out.println("Quantas hora voce trabalhou no mês: ");
+		double HoraTrabalhada = scan.nextDouble();
+		
+		double Salario = ValorHora * HoraTrabalhada;
+		
+		System.out.println("O Valor do Salario: " + Salario);
+		 
+
 
 	}
 
---
+++ loiane/src/aula13/Exer09.java
@@ -0,0 +1,10 @@
+package aula13;
+
+public class Exer09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
