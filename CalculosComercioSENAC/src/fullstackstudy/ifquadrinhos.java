3ab1828e6241a1528339a87bdb19ba8dbb039462
lopcarv <d7m7e7@gmail.com>
2026-02-06 15:31:36 -0300

    15h31

---
+++ CalculosComercioSENAC/src/fullstackstudy/ifquadrinhos.java
@@ -0,0 +1,14 @@
+package fullstackstudy;
+
+public class ifquadrinhos {
+
+	public static void main(String[] args) {
+		boolean temBatata = true;
+		int ovos = 6;
+		if (temBatata) {
+			ovos = 9;
+		}
+		System.out.println("Compre " + ovos + " ovos.");
+	}
+
+}
